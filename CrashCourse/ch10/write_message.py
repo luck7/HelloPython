7411bb7bd274c89df1907f8a6f62cfc33d501879
Jeff Fox <luck7@live.cn>
2021-01-14 21:32:32 +0800

    ch10

---
+++ CrashCourse/ch10/write_message.py
@@ -0,0 +1,15 @@
+filename = 'programming.txt'
+
+with open(filename, 'w') as file_object:
+	file_object.write("I love progrmming.\n")
+	file_object.write("I love creating new games.\n")
+
+with open(filename, 'a') as file_object:
+	file_object.write("I also love finding meaning in large datasets.\n")
+	file_object.write("I love creating apps that can run in a browser.\n")
+
+filename = 'programming.txt'
+with open(filename) as file_object:
+	lines = file_object.readlines()
+for line in lines:
+	print(line.strip())
