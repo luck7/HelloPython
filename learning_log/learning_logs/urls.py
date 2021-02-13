95054d6e8908834146716d1204c8d52d76f96c57
Jeff Fox <luck7@live.cn>
2021-02-13 22:52:02 +0800

    bug fix

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -5,11 +5,11 @@ from django.urls import path
 from . import views
 
 app_name = 'learning_logs'
-url_patterns = [
+urlpatterns = [
   # Home page
   path('', views.index, name='index'),
   # Page that shows all topics.
-  path('topics', views.topics, name='topics'),
+  path('topics/', views.topics, name='topics'),
   # Details page for a single topic.
   path('topics/<int:topic_id>/', views.topic, name='topic'),
 ]
